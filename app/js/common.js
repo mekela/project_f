5ce6157e669cd6fee5c31c7538f6a45273fb1a06
Alex Gerasimyuk <2011.sanya@gmail.com>
2019-08-12 00:14:01 +0300

    add news

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,56 @@
 $(document).ready(function() {
 	//menu
-	$( ".menu__trigger" ).click(function() {
+	$( ".menu__trigger" ).hover(function() {
 		$( ".menu" ).toggleClass( "active" );
 	});
+
+	//slick
+	$('.about-slider__section').slick({
+		dots: true,
+		infinite: true,
+		arrows: false,
+		speed: 300,
+		fade: true,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
+	$('.solutions__slider-block').slick({
+		dots: true,
+		infinite: true,
+		arrows: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
+	$('.testimonials__block').slick({
+		dots: true,
+		infinite: true,
+		arrows: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true
+	});
+
 });
+//paralax
+$(window).scroll( function() {
+	var scroll = $(window).scrollTop();
+    var speed = .9;
+    
+	$('.scroller').each(function(){
+		var $this = $(this);
+        var $parent = $this.parent();
+        var topOffset = $parent.offset().top;
+        var height = $parent.outerHeight(true);
+        var parallaxSize = (scroll - topOffset) * speed;
+        
+        // prevent parallax when scroll down
+        if(scroll > topOffset + height) {
+            return;
+        }
+		
+        $this.css({ 
+            'transform': scroll >= topOffset ? ('translateY(' + parallaxSize + 'px)' ) : ''
+        });
+	}); 
+}); 
